6d4c2bb5f2d22e3b89077d3d8c43a174205c5c5a
abhishekjain0112 <abhishekjain0112@gmail.com>
2021-06-02 16:48:09 +0530

    changed to openui

--- configmaster/src/main/java/com/ccm/configmaster/configmaster/controller/CcmConfigurationController.java
+++ configmaster/src/main/java/com/ccm/configmaster/configmaster/controller/CcmConfigurationController.java
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ccm.configmaster.configmaster.model.CcmConfiguration;
 import com.ccm.configmaster.configmaster.service.CcmConfigurationService;
 
 @RestController
+@RequestMapping("/api")
 public class CcmConfigurationController {
 
 	@Autowired
