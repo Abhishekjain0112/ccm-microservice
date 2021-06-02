72f83bb101a8d3994ea516a8fdcc24c139c21b38
abhishekjain0112 <abhishekjain0112@gmail.com>
2021-06-02 16:58:03 +0530

    Changed Doc Name

--- configmaster/src/main/java/com/ccm/configmaster/configmaster/ConfigmasterApplication.java
+++ configmaster/src/main/java/com/ccm/configmaster/configmaster/ConfigmasterApplication.java
@@ -3,10 +3,15 @@ package com.ccm.configmaster.configmaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 
 
 @SpringBootApplication
-
+@OpenAPIDefinition(
+		info = @Info(title = "CCM Master Application", version = "1.0.0")
+		)
 public class ConfigmasterApplication {
 
 	public static void main(String[] args) {
